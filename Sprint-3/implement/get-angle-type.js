87f3ea086bccba12bea267f2b2fb932c906c217f
Kazeem Hamzat <hamzatkazeem@yahoo.com>
2024-12-07 14:57:03 +0000

    Create get-angle-type
    Create get-angle-type

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -25,3 +25,37 @@
 // Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function getAngleType(angle_Value){
+
+  if (angle_Value==90) 
+    return "Right Angle"
+  else if (angle_Value < 90) 
+    return "Acute Angle"
+  else if (angle_Value > 90 && angle_Value < 180) 
+    return "Obtuse Angle"
+  else if (angle_Value ==180) 
+    return "Straight Angle"
+  else if (angle_Value > 180 && angle_Value <= 360) 
+    return "Reflex Angle"
+  else
+  return "Invalid Value"
+}
+
+// Accept input from the console
+rl.question('Please enter a value for angle between 0 and 360: ', (angle_Value) => {
+  ; // Pass the input to the function
+
+  console.assert(getAngleType(angle_Value) === "Right Angle", angle_Value, "entered. Angle value must be 90");
+  
+  rl.close();
+  
+});
+
