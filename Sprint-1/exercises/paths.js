b5b823fa2db2933d2d300352e412fa9842365291
Kazeem Hamzat <hamzatkazeem@yahoo.com>
2024-12-18 21:24:37 +0000

    Resolving Feedback issues on path.js

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -9,7 +9,7 @@
 
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 console.log()
-const filePath = "/users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
+const filePath = "/folder1/.git/folder2.secret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
@@ -19,15 +19,15 @@ console.log()
 
 // Create a variable to store the dir part of the filePath variable
 
-const indexOfEqual = filePath.indexOf("=");
-const dir=filePath.slice(indexOfEqual+2,lastSlashIndex)
+const indexOfFirstslash = filePath.indexOf("/");
+const dir=filePath.slice(indexOfFirstslash+1,lastSlashIndex)
 console.log(`The dir part of ${filePath} is ${dir}`);
 
 
 
 // Create a variable to store the ext part of the variable
 console.log(filePath.length)
-const indexOfFullStop = filePath.indexOf(".");
+const indexOfFullStop = filePath.lastIndexOf(".");
 const ext=filePath.slice(indexOfFullStop,filePath.length)
 console.log(`The ext part of ${filePath} is ${ext}`);
 console.log()
